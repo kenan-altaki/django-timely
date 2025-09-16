a8c2f872885a58954272ae606d3112fa99ee5b2a
Yazan <100113688+yaltaki@users.noreply.github.com>
2025-09-16 09:33:20 +0200

    _boilerplate

--- timely/config/urls.py
+++ timely/config/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path
+from django_timely.views import home
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("test/", home, name='home'),
 ]
--- timely/django_timely/views.py
+++ timely/django_timely/views.py
@@ -1,1 +1,15 @@
+from django.shortcuts import render
+import json
+
+
 # Create your views here.
+def home(request):
+    business_hours = [
+        {"daysOfWeek": [1, 2, 3], "startTime": "08:00", "endTime": "18:00"},
+        {"daysOfWeek": [4, 5], "startTime": "10:00", "endTime": "16:00"},
+    ]
+    return render(
+        request,
+        "django_timely/base.html",
+        {"business_hours": json.dumps(business_hours)},
+    )
